dd55a25fa426406c9d08da99212e994c3f42c2d0
Breno Brandão <breno.brandao@celeritech.com.br>
2018-03-23 15:36:22 -0300

    Bash return on run.

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ if [ "$SSH_PWD" != '' ]; then
 fi
 
 echo "Connecting to VPN"
-echo $VPN_PWD | openconnect -u "$VPN_USER" --passwd-on-stdin --authgroup="$VPN_GROUP" --servercert="$VPN_CERT" $VPN_IP
+echo $VPN_PWD | openconnect -u "$VPN_USER" --passwd-on-stdin --authgroup="$VPN_GROUP" --servercert="$VPN_CERT" $VPN_IP &
 
-exec "$@"
+exec /bin/bash
